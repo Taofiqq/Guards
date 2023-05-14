5527b843fcfffd494ae1116518aabc3912e3a122
Taofiq Aiyelabegan <61933245+Taofiqq@users.noreply.github.com>
2023-05-14 09:47:01 +0100

    implemented the auth guard

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AuthGuard } from './guards/auth.guard';
 
 @Controller()
+@UseGuards(AuthGuard)
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('test')
+  test(): string {
+    return 'This is a Test Route';
   }
 }
---
+++ src/guards/auth.guard.ts
@@ -0,0 +1,18 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const apiKey = request.headers;
+    if (apiKey && apiKey.api_key === 'MY_API_KEY') {
+      return true;
+    }
+    return false;
+  }
+}
